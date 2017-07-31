eda5aff2560a893119e0b5363d094df17ec2500c
Ryan Hellyer <ryanhellyer@gmail.com>
2017-07-31 13:11:33 +0200

    Initial plugin commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# woocommerce-default-number
+# WooCommerce Set Default Number of Products
 Changes the default number of products to purchase from 0 to 1
---
+++ woocommerce-default-number.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Plugin Name: WooCommerce Set Default Number of Products
+Plugin URI: https://geek.hellyer.kiwi/plugins/
+Description: Changes the default number of products to purchase from 0 to 1
+Author: Ryan Hellyer
+Version: 1.0
+Author URI: https://geek.hellyer.kiwi/
+
+Copyright 2017 Ryan Hellyer
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+*/
+
+
+add_filter( 'woocommerce_quantity_input_args', 'event_tickets_default_number' );
+function woocommerce_default_number( $args ) {
+	$args['input_value'] = 1;
+	return $args;
+}
